34c3272ffb7bac73a073617eb52f2d472f69ba47
GuilhermeBorges3Ddev <gsilvaborges@gmail.com>
2019-03-16 20:41:16 -0300

    Eva.js component was created to be used in our first default component, called App.js

---
+++ projeto-inicial/src/Eva.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class Eva extends Component {
+  render() {
+    return (
+      <div>
+        I'm the first component after the App.js component...  
+      </div>
+    );
+  }
+}
+
+export default Eva;
