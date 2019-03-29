362158aa2657539909262caa8031606a9a6681ef
GuilhermeBorges3Ddev <gsilvaborges@gmail.com>
2019-03-29 00:02:10 -0300

    Button created on UserForm.js, who calls the handleSubmit and the submitUserAction methods

--- projeto-inicial/src/pages/user/UserForm.js
+++ projeto-inicial/src/pages/user/UserForm.js
@@ -1,14 +1,21 @@
-import {reduxForm} from 'redux-form';
+import {reduxForm, Field} from 'redux-form';
 import {connect} from 'react-redux';
 import React from 'react';
 
 const UserFormFunc = props => {
+    
+    const {handleSubmit} = props
+    
+    const submit = (data, submitUserAction) => {
+        submitUserAction(data)
+    }
+
     return (
-        <form>
+        <form onSubmit={handleSubmit((fields) => submit(fields, submitUserAction))}>
             <label>
                 Name
             </label>
-            <input
+            <Field
                 type="text"
                 component="input"
                 name="name"
@@ -16,11 +23,12 @@ const UserFormFunc = props => {
             <label>
                 Email
             </label>
-            <input
+            <Field
                 type="text"
                 component="input"
                 name="email"
             />
+            <button type="submit">Submit</button>
         </form>
     );
 }
