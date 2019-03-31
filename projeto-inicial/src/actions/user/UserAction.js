03504c733736fdb8b05e9a61aafc519c275abc0f
GuilhermeBorges3Ddev <gsilvaborges@gmail.com>
2019-03-31 03:19:12 -0300

    UserAction.js and UserForm.js has now two new concepts to understand the aplication of these two components

--- projeto-inicial/src/actions/user/UserAction.js
+++ projeto-inicial/src/actions/user/UserAction.js
@@ -1,3 +1,6 @@
+/* 
+    These action is used only in UserAction.js
+*/
 
 export const submitUserAction = (data) => {
     console.log(data)
--- projeto-inicial/src/pages/user/UserForm.js
+++ projeto-inicial/src/pages/user/UserForm.js
@@ -3,6 +3,11 @@ import {connect} from 'react-redux';
 import React from 'react';
 import {submitUserAction} from "../../actions/user/UserAction";
 
+/*
+    These form manipulates some states for trainning 
+    the Redux lib concepts, its not a 'props-area' or a 'state area'.
+*/
+
 const UserFormFunc = props => {
     
     const {handleSubmit} = props
@@ -38,8 +43,6 @@ const UserForm = (reduxForm({
     form: 'formUser'
 }))(UserFormFunc)
 
-const mapStateToProps = state => ({
-    
-})
+const mapStateToProps = state => ({})
 
 export default connect(mapStateToProps, {submitUserAction})(UserForm)
