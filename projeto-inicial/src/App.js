1379435d0646973321c2236e3cf6080e9bbed3fa
GuilhermeBorges3Ddev <gsilvaborges@gmail.com>
2019-07-14 23:06:58 -0300

    Organizing the project replacing images into a correct place

--- projeto-inicial/src/App.js
+++ projeto-inicial/src/App.js
@@ -1,112 +1,116 @@
-import React, { Component } from 'react';
-import './App.css';
-import logo from './logo.svg';
-import Eva from './Eva';
+import React, { Component } from "react";
+import "./App.css";
+import logo from "./img/logo.svg";
+import Eva from "./Eva";
 //Components to do a route-map
-import { Link, Route} from 'react-router-dom'
-import routesConfig from './routesConfig';
+import { Link, Route } from "react-router-dom";
+import routesConfig from "./routesConfig";
 
 class App extends Component {
-
   //Constructor was build to manipulate the state of the father component
-  constructor(props){
-    super(props)
+  constructor(props) {
+    super(props);
     this.state = {
       name: "Eva.js component",
       email: "eva.official@gmail.com"
-    }
+    };
     //These two methods give a state-change into two buttons component
-    this.changeState = this.changeState.bind(this)
-    this.resetState = this.resetState.bind(this)
-    //This other method are used to change the 'value' of each form section 
-    this.changeInput = this.changeInput.bind(this)
+    this.changeState = this.changeState.bind(this);
+    this.resetState = this.resetState.bind(this);
+    //This other method are used to change the 'value' of each form section
+    this.changeInput = this.changeInput.bind(this);
   }
 
   //Modifying the state on event 'onClick()'
-  changeState(){
+  changeState() {
     this.setState({
       name: "Eva.js is the main component, state CHANGED"
-    })
+    });
   }
 
-  resetState(){
+  resetState() {
     this.setState({
       name: "Eva.js component"
-    })
+    });
   }
 
-//Email and name change dinamically when an digit action is active
-  changeInput(event){
-    let target = event.target
-    let index = target.name
+  //Email and name change dinamically when an digit action is active
+  changeInput(event) {
+    let target = event.target;
+    let index = target.name;
     this.setState({
-      [index]:target.value
-    })
+      [index]: target.value
+    });
   }
 
   render() {
     return (
-    <div className="Routering">
-      <div className="App">  
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          {/* Configurating the route*/}
+      <div className="Routering">
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            {/* Configurating the route*/}
             <table>
               <tr>
-                  <th>
-                    <Link to="/" style={{ color: 'black' }}>Home</Link>
-                  </th>
-                  <th></th>
-                  <th>
-                    <Link to="/user" style={{ color: 'black' }}>User</Link>
-                  </th>
+                <th>
+                  <Link to="/" style={{ color: "black" }}>
+                    Home
+                  </Link>
+                </th>
+                <th />
+                <th>
+                  <Link to="/user" style={{ color: "black" }}>
+                    User
+                  </Link>
+                </th>
               </tr>
             </table>
-          <p>
-             Project React.JS mounted using NodeJS!
-          </p>
-          <Eva></Eva>
-          <div>
-            <form>
-                <label>Name:
+            <p>Project React.JS mounted using NodeJS!</p>
+            <Eva />
+            <div>
+              <form>
+                <label>
+                  Name:
                   <div className="mx-2">
-                      <input
-                        type="text" 
-                        name="name"
-                        value={this.state.name}
-                        onChange={this.changeInput}
-                      ></input>
-                    </div>
+                    <input
+                      type="text"
+                      name="name"
+                      value={this.state.name}
+                      onChange={this.changeInput}
+                    />
+                  </div>
                 </label>
-                <label>Email:
+                <label>
+                  Email:
                   <div className="mx-2">
-                      <input 
-                        type="email" 
-                        name="email"
-                        value={this.state.email}
-                        onChange={this.changeInput}
-                      ></input>
-                   </div>  
+                    <input
+                      type="email"
+                      name="email"
+                      value={this.state.email}
+                      onChange={this.changeInput}
+                    />
+                  </div>
                 </label>
-            </form>
-            {this.state.name} - {this.state.email}
-          </div>
-          <div>
-            <button onClick={this.changeState}>Change state</button>
-            <button onClick={this.resetState}>Reset state</button>
-          </div>
-        </header>
-       
+              </form>
+              {this.state.name} - {this.state.email}
+            </div>
+            <div>
+              <button onClick={this.changeState}>Change state</button>
+              <button onClick={this.resetState}>Reset state</button>
+            </div>
+          </header>
+        </div>
+        {routesConfig.map((value, key) => {
+          return (
+            <Route
+              key={key}
+              path={value.path}
+              component={value.component}
+              exact={value.exact}
+            />
+          );
+        })}
       </div>
-      {routesConfig.map((value, key) => {
-        return <Route
-          key={key}
-          path={value.path}
-          component={value.component}
-          exact={value.exact} 
-        ></Route>
-      })}
-    </div>  
     );
   }
 }
--- projeto-inicial/src/Eva.js
+++ projeto-inicial/src/Eva.js
@@ -1,13 +1,13 @@
-import React, { Component } from 'react'
-import User from './User';
-import photo from './evaPhoto.png';
+import React, { Component } from "react";
+import User from "./User";
+import photo from "./img/evaPhoto.png";
 
 class Eva extends Component {
   render() {
     return (
       <div>
-        I'm the first component after the App.js component... 
-        <User name="Eva.js component" photo={photo} ></User>
+        I'm the first component after the App.js component...
+        <User name="Eva.js component" photo={photo} />
       </div>
     );
   }
